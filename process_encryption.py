b85478bc31751bd80e394029e6b52a3d58ec6655
its22647 <its22647.622@gmail.com>
2025-09-18 11:20:16 +0500

    ARSS

--- process_encryption.py
+++
@@ -1,6 +0,0 @@
-import time, os
-p = os.path.expanduser("~/Documents/test_encrypt.txt")
-while True:
-    with open(p, 'a') as f:
-        f.write("0"*10000)
-    time.sleep(0.01)
--- update_clamav.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def update_clamav():
-    try:
-        subprocess.run(["freshclam"], check=True)
-        print("✅ ClamAV database updated successfully.")
-    except subprocess.CalledProcessError:
-        print("❌ Failed to update ClamAV database.")
